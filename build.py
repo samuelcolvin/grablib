e9bc2da6a15135ede7aa2e221427a94e0a4cf362
samuelcolvin <s@muelcolvin.com>
2014-04-01 18:14:47 +0100

    changed version setting

--- build.py
+++ build.py
@@ -14,6 +14,10 @@ setup_text = open('setup.py','r').read()
 s=re.search("version *= *'(.+?)'", setup_text)
 v = s.groups()[0]
 print 'setting version to: %s' % v
-init_file = 'GrabLib/_version.py'
-init_text = "__version__ = 'v%s'\n" % v
-open(init_file,'w').write(init_text)
+init_file = 'GrabLib/__init__.py'
+init_text = open(init_file, 'r').read()
+init_text, subcount = re.subn('__version__ .*', "__version__ = 'v%s'" % v, init_text)
+if subcount == 0:
+    print 'WARNING: failed to set version in %s' % init_file
+else:
+    open(init_file,'w').write(init_text)
