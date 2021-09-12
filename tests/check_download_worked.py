ff3a9d90ff672e0745a4bcf8fc7642a9fcd90265
Samuel Colvin <s@muelcolvin.com>
2021-09-12 14:07:14 +0100

    fix download check script

--- tests/check_download_worked.py
+++ tests/check_download_worked.py
@@ -5,18 +5,18 @@ from pathlib import Path
 
 def check_file(f: str):
     if not Path(f).is_file():
-        print(f'file "{f}" does not exist')
+        print(f'"{f}" is not a file')
         sys.exit(2)
 
 
 def check_dir(f: str):
     if not Path(f).is_dir():
-        print(f'dir "{f}" does not exist')
+        print(f'"{f}" is not a directory')
         sys.exit(2)
 
 
 if __name__ == '__main__':
     check_file('.grablib.lock')
     check_dir('static')
-    check_dir('static/libs/build_bootstrap.scss')
-    check_dir('static/libs/js/jquery.js')
+    check_file('static/libs/build_bootstrap.scss')
+    check_file('static/libs/js/jquery.js')
